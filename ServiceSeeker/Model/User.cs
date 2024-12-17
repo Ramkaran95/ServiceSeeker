6f550629102637136c5093ea763a4ffb04eeb7f3
Admin <Admin@DESKTOP-KU1I1VE>
2024-12-17 15:04:17 +0530

    Add project files.

---
+++ ServiceSeeker/Model/User.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+namespace ServiceSeeker.Model
+{
+    public class User
+    {
+        public required int UserId { get; set; }
+
+        public required String UserName { get; set; }
+        public required String FirstName { get; set; }
+        public required String  MiddleName { get; set; }
+        public required String LastName { get; set; }
+        public required int PhoneNumber { get; set; }
+        public required int Email { get; set; }
+
+        public required int Password { get; set; }
+        public String? ProfilePhoto { get; set; } ="Default.jpg";
+
+        public required DateTime CreatAt { get; set; }=DateTime.Now; 
+
+    }
+}
